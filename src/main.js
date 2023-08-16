57f5bf5429255dc0f8df799838b372df917aff7c
limyzi <930716a@gmail.com>
2023-08-16 14:05:02 +0900

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-// Header Scrolling
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
+// header scroll dark css
 document.addEventListener('scroll', (event) => {
   if(window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
+  }
+})
+
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+// home scroll transparent css
+document.addEventListener('scroll', (event) => {
+  if(homeHeight > window.scrollY) {
+    home.style.opacity = (homeHeight - window.scrollY)/homeHeight;
   }
 })
